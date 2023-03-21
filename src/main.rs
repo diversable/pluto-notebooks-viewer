9055a0e88761ecfa7151e71f4c89e109de9d26a7
Diversable <dan.mantei@outlook.com>
2023-03-21 12:28:05 -0700

    clean up

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,42 @@ use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
+        platform::run_return::EventLoopExtRunReturn,
         window::WindowBuilder,
     },
     webview::WebViewBuilder,
 };
 
-use std::env;
 use std::process::Command;
 use std::thread;
+use std::time;
 
 fn main() -> wry::Result<()> {
-    // println!(
-    //     "input the Pluto notebook secret as the first arg to this program, if necessary...\n\n"
-    // );
-
-    let _julia_handler = thread::spawn(|| {
+    let julia_handler = thread::spawn(|| {
         Command::new("julia")
             // .env("JULIA_DIR", "~/.juliaup/bin/")
             // .env("LD_LIBRARY_PATH", "~/.julia/juliaup/julia-1.8.5+0.x64.linux.gnu/lib/")
-            .env("JULIA_NUM_THREADS", "16")
+            // .env("JULIA_NUM_THREADS", "16")
             .arg("-E")
-            .arg("using Pluto; Pluto.run()")
+            .arg("using Pluto; Pluto.run(; launch_browser=false, require_secret_for_access=false )")
             .status()
             // .output()
             .expect("could not start Pluto server... Make sure Pluto is properly installed..");
     });
 
+    let _start_pluto = thread::sleep(time::Duration::from_secs(5));
+
     let mut base_url = String::from("http://127.0.0.1:1234/");
 
-    let args: Vec<String> = env::args().collect();
-
-    // the first arg is always the prog name
-    // if the length of the args vector is
-    if &args.len() > &1_usize {
-        let secret = &args[1];
-
-        let secret_query = "&secret=";
-
-        base_url.push_str(secret_query);
-
-        base_url.push_str(secret);
-        dbg!(&base_url);
-    }
-
     // Start the UI
-    let event_loop = EventLoop::new();
+    let mut event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Pluto Notebooks")
         .build(&event_loop)?;
     let _webview = WebViewBuilder::new(window)?.with_url(&base_url)?.build()?;
 
-    // julia_handler.join().unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
+    // use the `run_return` method instead of simply `run` so that you can re-join the julia handler thread / clean up before the program exits..
+    event_loop.run_return(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
@@ -69,9 +52,14 @@ fn main() -> wry::Result<()> {
                 // _julia_handler
                 //     .join()
                 //     .expect("Couldn't finish Julia / Pluto");
-                *control_flow = ControlFlow::Exit;
+                *control_flow = ControlFlow::ExitWithCode(1);
+
+                // *control_flow = ControlFlow::Exit;
             }
             _ => (),
         }
     });
+
+    julia_handler.join().unwrap();
+    Ok(())
 }
